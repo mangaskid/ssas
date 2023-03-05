b082e896737b37571d34d0669523945ad608c16c
mangaskid <aliyuhaidarmanga@gmail.com>
2023-03-05 16:01:18 +0100

    Auth corrected for students

--- main/models.py
+++ main/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser 
 
 # Create your models here.
 
-class User(AbstractUser):
+class User(AbstractBaseUser):
     is_student = models.BooleanField(default=False)
     is_supervisor = models.BooleanField(default=False)
     phone = models.CharField(max_length=255)
