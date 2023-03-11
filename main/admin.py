cb0b390213be768719af260c7226fe870b9349aa
mangaskid <aliyuhaidarmanga@gmail.com>
2023-03-11 19:41:39 +0100

    super admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import User, SiwesReg, Attendance
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(SiwesReg)
+admin.site.register(Attendance)
